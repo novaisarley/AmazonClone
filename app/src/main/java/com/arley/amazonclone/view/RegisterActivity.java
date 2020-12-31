31d6f69d488487cea5fbc46b6b9dc36d5dfaf270
Arley Novais <novaisarle@gmail.com>
2020-12-30 22:29:08 -0400

    Add register screen

---
+++ app/src/main/java/com/arley/amazonclone/view/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.arley.amazonclone.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.arley.amazonclone.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
